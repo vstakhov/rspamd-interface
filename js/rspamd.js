b1ed1c49a36e463eff0bbc464969b8fa93ac11ba
Vsevolod Stakhov <vsevolod@highsecure.ru>
2015-09-23 13:27:46 +0100

    Fix style of symbol weights.

--- js/rspamd.js
+++ js/rspamd.js
@@ -551,10 +551,10 @@
                                 min = item.weight * 2;
                             }
                             items.push('	<div class="row-fluid row-bordered" data-slider="hover">' +
-                                '<label class="span5" for="' + item.symbol + '" title="' + item.description + '">' +
-                                '<code>' + item.symbol + '</code><small class="symbol-description">' + item.description + '</small>' +
+                                '<label class="span7" for="' + item.symbol + '" title="' + item.description + '">' +
+                                '<code>' + item.symbol + '</code><p class="symbol-description">' + item.description + '</p>' +
                                 '</label>' +
-                                '<div class="span4 spin-cell">' +
+                                '<div class="span3 spin-cell">' +
                                 '<input class="numeric" data-role="numerictextbox" autocomplete="off" "type="number" class="input-mini" min="' +
                                 min + '" max="' +
                                 max + '" step="' + decimalStep(item.weight) +
